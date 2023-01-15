d4a31b589cee983aa6403a554fcb59d55c01ff64
kubafurtacz <yetan96@gmail.com>
2023-01-15 12:48:41 +0100

    passed cw1

--- src/main/java/cw1/Solution.java
+++ src/main/java/cw1/Solution.java
@@ -1,13 +1,23 @@
 package cw1;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
 
     public int solution(int number) {
+        Integer sum =0;
         if(number<0){
-            return 0;
+            sum= 0;
         }
         else{
-        return number;
+            Set<Integer> numbers = new HashSet<>();
+            for (int i = 0; i < number; i++) {
+                if (i%3==0||i%5==0) numbers.add(i);
+            }
+             sum = numbers.stream()
+                    .reduce(0, Integer::sum);
         }
+        return sum;
     }
 }
--- src/test/java/cw1/SolutionTest.java
+++ src/test/java/cw1/SolutionTest.java
@@ -1,9 +1,18 @@
 package cw1;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class SolutionTest {
     @Test
     public void test() {
         assertEquals(23, new Solution().solution(10));
+    }
+
+    @Test
+    public void negativeTest(){
+        assertEquals(0, new Solution().solution(-1));
     }
 
 }
