5ece5239a65ac3138e1bc327de2ed7547a78343d
kubafurtacz <yetan96@gmail.com>
2023-01-15 12:28:56 +0100

    created structure for cw

---
+++ src/main/java/cw1/Solution.java
@@ -0,0 +1,13 @@
+package cw1;
+
+public class Solution {
+
+    public int solution(int number) {
+        if(number<0){
+            return 0;
+        }
+        else{
+        return number;
+        }
+    }
+}
---
+++ src/test/java/cw1/SolutionTest.java
@@ -0,0 +1,9 @@
+package cw1;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        assertEquals(23, new Solution().solution(10));
+    }
+
+}
